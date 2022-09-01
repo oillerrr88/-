3245cef4e083e95d78e51134bbc1318088d4d4d9
pari <107874349+oillerrr88@users.noreply.github.com>
2022-09-01 15:21:09 +0700

    Add files via upload
    คำสั่ง input ชื่อ อายุ

---
+++ 1965.py
@@ -0,0 +1,8 @@
+name = input("คุณชื่ออะไร : ")
+age = int(input("คุณอายุเท่าไหร่: "))
+weight = float(input("ป้อนน้ำหนักคูณ: "))
+hight = int(input("ป้อนส่วนสุงคูณ"))
+
+
+
+print("ชื่อ",name,"อายุ",age,"น้ำหนัก",weight,"ส่วนสูง",hight)
