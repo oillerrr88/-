cb3a790aa791f4b1d14c75eabd275e189cbb77f3
pari <107874349+oillerrr88@users.noreply.github.com>
2022-09-01 15:08:35 +0700

    Add files via upload
    คำสั่ง input ชื่อ อายุ น้ำหนัก

---
+++ gui1.py
@@ -0,0 +1,19 @@
+from logging import root
+import tkinter
+from turtle import bgcolor
+
+bgcolor("black")
+root = tkinter.Tk()
+root.title("Wee")
+
+root.geometry("800x600")
+
+
+
+
+
+root.mainloop()
+
+
+
+
